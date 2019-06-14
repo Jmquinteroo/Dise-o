0ded2154f99739dddb9275d647cb49ec7eda70fa
jmquinteroo <jmquinteroo@unal.edu.co>
2019-06-13 19:50:43 -0500

    Primer commit con la instalacón

---
+++ app/Models/Lugar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lugar extends Model
+{
+    //
+    protected $table = 'lugares';
+    protected $fillable = [
+        'name'
+    ];
+
+
+}
